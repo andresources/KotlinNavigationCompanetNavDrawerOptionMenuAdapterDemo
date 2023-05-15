2836ddb5aeff136c5abf9e13d0ade76ee9dc4bc6
Hari <andresources1039@gmail.com>
2023-05-15 17:59:38 +0530

    Java

---
+++ app/src/main/java/com/kotlinnavigationdemo/References.java
@@ -0,0 +1,5 @@
+package com.kotlinnavigationdemo;
+public class References {
+    //https://developer.android.com/codelabs/android-databinding#1
+    //https://github.com/android/databinding-samples/tree/main
+}
