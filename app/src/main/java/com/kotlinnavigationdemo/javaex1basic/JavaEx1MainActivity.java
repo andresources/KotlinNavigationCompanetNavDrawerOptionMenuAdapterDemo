ba9d7dc5119fa90145dd8a87a953454064fe9d0d
Hari <andresources1039@gmail.com>
2023-05-15 17:52:38 +0530

    Java

---
+++ app/src/main/java/com/kotlinnavigationdemo/javaex1basic/JavaEx1MainActivity.java
@@ -0,0 +1,15 @@
+package com.kotlinnavigationdemo.javaex1basic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kotlinnavigationdemo.R;
+
+public class JavaEx1MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_java_ex1_main);
+    }
+}
